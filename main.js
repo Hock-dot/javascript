3b8204b7e752bc6dcdf1394b9d7c6b8b786c0083
Hock-dot <82542120+Hock-dot@users.noreply.github.com>
2021-04-27 08:31:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let usuario=prompt("Ingrese su usuario");
+let contraseña=prompt("Ingrese la contraseña");
+let codigoempresa=prompt("Ingrese su codigo unico establecido")
+
+if (usuario=="Henry", contraseña==2222, codigoempresa==0605652882){
+    alert("Acceso permitido, Accediendo al sistema");
+}
+else{
+	alert("Acceso denegado")
+}
+
+
+
